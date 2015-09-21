76b00c3fd8f9d3b81a0e9dd305fbb6d60f85c87f
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-09-21 13:57:48 +0800

    sdf

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -8,13 +8,50 @@
 	}
 
 	//ajax返回
-function ajaxReturn($errcode = 0, $msg = '', $data = array()){
-        $data = array('status'=>$errcode, 'msg'=>$msg, 'data'=>$data);
-        exit(json_encode($data));
-    }
-	//微信
-	function is_weixin(){   
-		return strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false;
+	function ajaxReturn($errcode = 0, $msg = '', $data = array()){
+	        $data = array('status'=>$errcode, 'msg'=>$msg, 'data'=>$data);
+	        exit(json_encode($data));
+	    }
+		//微信
+		function is_weixin(){   
+			return strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false;
+		}
+		function isAjax() {
+	    return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && 'xmlhttprequest' == strtolower($_SERVER['HTTP_X_REQUESTED_WITH']);
+	}
+
+
+
+
+	function errReturn($errCode){
+	    ajaxReturn($errCode, Errorcode::getErrorMsg($errCode));
+	}
+
+
+	function get_client_ip(){
+	    $type       =  $type ? 1 : 0;
+	    static $ip  =   NULL;
+	    if ($ip !== NULL) return $ip[$type];
+	    if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+	        $arr    =   explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+	        $pos    =   array_search('unknown',$arr);
+	        if(false !== $pos) unset($arr[$pos]);
+	        $ip     =   trim($arr[0]);
+	    }elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+	        $ip     =   $_SERVER['HTTP_CLIENT_IP'];
+	    }elseif (isset($_SERVER['REMOTE_ADDR'])) {
+	        $ip     =   $_SERVER['REMOTE_ADDR'];
+	    }
+	    // IP地址合法验证
+	    $long = sprintf("%u",ip2long($ip));
+	    $ip   = $long ? array($ip, $long) : array('0.0.0.0', 0);
+	    return $ip[$type];
+	}
+
+	function checkApiServerIp(){
+	    $ip = get_client_ip();
+	    $valid = array('115.28.147.141','114.215.156.219');
+	    return in_array($ip, $valid);
 	}
 
 ?>
