f6d1c2a4279a0884250f123d20544de29e9682b8
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-09-22 14:04:25 +0800

    asdf

--- Application/Stage/Controller/GactorController.class.php
+++ Application/Stage/Controller/GactorController.class.php
@@ -11,7 +11,7 @@ class GactorController extends ComController {
     	//分页
 
 		$count = $actors->order('votes desc')->count();// 查询满足要求的总记录数
-		$Page  = new \Think\Page($count,1);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+		$Page  = new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数(25)
 		$show  = $Page->show();// 分页显示输出// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
 		$actorsval  = $actors->order('votes desc')
 				->limit($Page->firstRow.','.$Page->listRows)->select();
