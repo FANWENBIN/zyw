44cf635d905d0c83c534e0ace27fd813ae759b40
yangronghai <ronghai.yang@nadoo.cn>
2015-09-23 16:13:45 +0800

    update

--- zyw-web/js/app.js
+++ zyw-web/js/app.js
@@ -6,5 +6,8 @@ define(function (require, exports, module) {
   var nd = require("unit/nd.base");
   require("unit/jq.$touch");
   require("unit/jq.$pageTo");
-
+  alert()
+  $(function(){
+    alert()
+  })
 });
