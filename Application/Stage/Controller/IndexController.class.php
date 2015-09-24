f70f97470ae04b9bed153e3fe78884d5d75f4deb
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-09-24 17:08:12 +0800

    asdf

--- Application/Stage/Controller/IndexController.class.php
+++ Application/Stage/Controller/IndexController.class.php
@@ -42,7 +42,7 @@ class IndexController extends ComController {
     //修改密码
     public function uppasswd(){
         $oldp = I('get.oldp','','md5');
-        $data['newp'] = I('get.newp','','md5');
+        $data['newp'] = I('get.newp');
         $admin = M('admin');
         $where = array(
             'name'   =>session('name'),
