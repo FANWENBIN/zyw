4b3a6c89eb277b47b898ca7fcd089fa256fb00ee
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-09-25 16:25:43 +0800

    asdf

--- Public/sxx.js
+++ Public/sxx.js
@@ -1,5 +1,5 @@
 
-var webPath = "http://localhost/zyw";
+var webPath = "http://m2.nadoo.cn/p/zyw";
 var imgUploadFile = ""; // 图片上传后的文件夹目录
 var imgUploadServer = ""; // 图片上传接口地址
 var swfUrl = "__PUBLIC__/statics/swf"; // flash文件夹目录
