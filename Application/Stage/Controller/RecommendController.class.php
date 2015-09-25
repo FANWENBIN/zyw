f55ce9de80055724aa551c014d9ee76149599342
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-09-25 16:24:01 +0800

    asdf

--- Application/Stage/Controller/RecommendController.class.php
+++ Application/Stage/Controller/RecommendController.class.php
@@ -28,26 +28,32 @@ class RecommendController extends ComController {
     date：2015年9月23日15:58:17
     */
     public function addcommend(){
-        $data['type'] = I('judge');
-        $data['name'] = I('name');
-        $this->checkDump($data);//检查数据
-        $upload = new \Think\Upload();// 实例化上传类   
-        $upload->maxSize   =     3145728 ;// 设置附件上传大小   
-        $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型  
-        $upload->savePath  =      '/commend/'; // 设置附件上传目录    
-        // 上传文件   
-        $info   =   $upload->upload();    
-        if(!$info) {// 上传错误提示错误信息      
-            $this->error($upload->getError()); 
-            exit;  
-        }else{// 上传成功  
-            $data['img'] = $info['photo']['savepath'].$info['photo']['savename'];
-            $commend = M('recommend');
-            $sign = $commend->add($data);
-            if($sign){
-                $this->success('添加成功',U('Gactor/index'));
-            }else{
-                $this->error('添加失败');
+        $submit = I('post.submit');
+        if(empty($submit)){
+            $this->display();
+        }else{
+            $data['type'] = I('post.type');
+            $data['name'] = I('post.name');
+            $this->checkDump($data);//检查数据
+            $upload = new \Think\Upload();// 实例化上传类   
+            $upload->maxSize   =     3145728 ;// 设置附件上传大小   
+            $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型  
+            $upload->savePath  =      '/commend/'; // 设置附件上传目录    
+            // 上传文件   
+            $info   =   $upload->upload();    
+            if(!$info) {// 上传错误提示错误信息      
+                $this->error($upload->getError()); 
+                exit;  
+            }else{// 上传成功  
+                $data['img'] = $info['photo']['savepath'].$info['photo']['savename'];
+                $commend = M('recommend');
+
+                $sign = $commend->add($data);
+                if($sign){
+                    $this->success('添加成功',U('Recommend/index'));
+                }else{
+                    $this->error('添加失败');
+                }
             }
         }
     }
@@ -68,18 +74,23 @@ class RecommendController extends ComController {
             $data['name'] = I('post.name');
             $data['type'] = I('post.type');
             $id     = I('post.recommendid');
+
             $this->checkDump($data);
+
             $upload = new \Think\Upload();// 实例化上传类   
             $upload->maxSize   =     3145728 ;// 设置附件上传大小   
             $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型  
-            $upload->savePath  =      '/stage/images/'; // 设置附件上传目录    
+            $upload->savePath  =      '/commend/images/'; // 设置附件上传目录    
             // 上传文件   
-            $info   =   $upload->upload();    
+            $info   =   $upload->upload();   
+
+           
             if(!$info) {// 上传错误提示错误信息      
                
                 $sign = $commend->where('id='.$id)->save($data);
                 if($sign){
-                    $this->success('修改成功',U('Gactor/index'));
+                    
+                    $this->success('修改成功',U('Recommend/index'));
                 }else{
                     $this->error('没做任何修改');
                 }
@@ -88,7 +99,7 @@ class RecommendController extends ComController {
                 $data['img'] =  $info['photo']['savepath'].$info['photo']['savename'];
                 $sign = $commend->where('id='.$id)->save($data);
                 if($sign){
-                    $this->success('修改成功',U('Gactor/index'));
+                    $this->success('修改成功',U('Recommend/index'));
                 }else{
                     $this->error('没做任何修改');
                 }
