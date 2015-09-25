247068bc564822124316eb9ad015f77944c34c08
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-09-25 16:24:16 +0800

    asdf

---
+++ Public/sxx.js
@@ -0,0 +1,7 @@
+
+var webPath = "http://localhost/zyw";
+var imgUploadFile = ""; // 图片上传后的文件夹目录
+var imgUploadServer = ""; // 图片上传接口地址
+var swfUrl = "__PUBLIC__/statics/swf"; // flash文件夹目录
+var mp3UploadServer = ""; // mp3上传接口地址
+var mp3UploadFile = ""; // mp3上传后的文件夹目录
