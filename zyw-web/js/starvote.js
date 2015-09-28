cf6bd2c03bd2458bd4c504921dea03c5097777e3
yangronghai <ronghai.yang@nadoo.cn>
2015-09-28 13:03:22 +0800

    update

--- zyw-web/js/starvote.js
+++ zyw-web/js/starvote.js
@@ -7,154 +7,71 @@ define(function (require, exports, module) {
     var nd = app.nd;
     $(function () {
         var page = {
-            init: function(){
+            init: function () {
                 $("#topBack").touchend(page.backToPreviewPage);
-                $("#groupSex").touchend("li",page.changeGroupSex);
-                $("#groupColor").touchend("li",page.changeGroupColor)
+                $("#groupSex").touchend("li", page.changeGroup);
+                $("#groupColor").touchend("li", page.changeGroup);
+                page.getRenew(1, "redgroup")
             },
 
             //返回上一页
-            backToPreviewPage:function(){
+            backToPreviewPage: function () {
                 window.history.go(-1);
             },
-            //获取男女星分组列表
-            changeGroupSex: function(){
+            //获取分组列表
+            changeGroup: function () {
                 var $groupS = $("#groupSex");
                 var $groupC = $("#groupColor");
-                $groupS.find("li").removeClass("active");
+                $(this).parent().find("li").removeClass("active");
                 $(this).addClass("active");
-
-                if($groupC.find(".active").hasClass("red")){
-                    if($(this).hasClass("male")){
-                        //获取男性明星列表
-                        nd.ajax({
-                            url: "_api.php",
-                            data: { what: "abc"},
-                            success: function(e){
-                                // do success
-                            }
-                        });
-                    }else{
-                        //获取女性明星列表
-                        nd.ajax({
-                            url: "_api.php",
-                            data: { what: "abc"},
-                            success: function(e){
-                                // do success
-                            }
-                        });
-                    }
-                    //获取蓝组明星
-                }else if($groupC.find(".active").hasClass("blue")){
-                    if($(this).hasClass("male")){
-                        //获取男性明星列表
-                        nd.ajax({
-                            url: "_api.php",
-                            data: { what: "abc"},
-                            success: function(e){
-                                // do success
-                            }
-                        });
-                    }else{
-                        //获取女性明星列表
-                        nd.ajax({
-                            url: "_api.php",
-                            data: { what: "abc"},
-                            success: function(e){
-                                // do success
-                            }
-                        });
-                    }
-                }else{
-                    if($(this).hasClass("male")){
-                        //获取男性明星列表
-                        nd.ajax({
-                            url: "_api.php",
-                            data: { what: "abc"},
-                            success: function(e){
-                                // do success
-                            }
-                        });
-                    }else{
-                        //获取女性明星列表
-                        nd.ajax({
-                            url: "_api.php",
-                            data: { what: "abc"},
-                            success: function(e){
-                                // do success
-                            }
-                        });
-                    }
-                }
+                var _sex = $groupS.find("active").data("sex");
+                var _color = $groupS.find("active").data("color");
+                //获取明星列表
+                page.getRenew()
 
             },
-            //获取分组列表
-            changeGroupColor: function(){
-                var $groupC = $("#groupColor");
-                $groupC.find("li").removeClass("active");
-                $(this).addClass("active");
-                //男演员分组
-                if($("#groupSex").find(".active").hasClass("male")){
-                    if($(this).hasClass("red")){
-                        //获取红组明星列表
-                        nd.ajax({
-                            url: "_api.php",
-                            data: { what: "abc"},
-                            success: function(e){
-                                // do success
-                            }
-                        });
-                    }else if($(this).hasClass("blue")){
-                        //获取蓝组明星列表
-                        nd.ajax({
-                            url: "_api.php",
-                            data: { what: "abc"},
-                            success: function(e){
-                                // do success
-                            }
-                        });
-                    }else{
-                        //获取绿组明星列表
-                        nd.ajax({
-                            url: "_api.php",
-                            data: { what: "abc"},
-                            success: function(e){
-                                // do success
-                            }
-                        });
-                    }
-                }else{
-                    //女演员分组
-                    if($(this).hasClass("red")){
-                        //获取红组明星列表
-                        nd.ajax({
-                            url: "_api.php",
-                            data: { what: "abc"},
-                            success: function(e){
-                                // do success
-                            }
-                        });
-                    }else if($(this).hasClass("blue")){
-                        //获取蓝组明星列表
-                        nd.ajax({
-                            url: "_api.php",
-                            data: { what: "abc"},
-                            success: function(e){
-                                // do success
-                            }
-                        });
-                    }else{
-                        //获取绿组明星列表
-                        nd.ajax({
-                            url: "_api.php",
-                            data: { what: "abc"},
-                            success: function(e){
-                                // do success
-                            }
-                        });
-                    }
-                }
+            getRenew: function (_sex, _color) {
+                nd.ajax({
+                    type: "get",
+                    dataType: "json",
+                    data: {
+                        url: "/index.php?m=Home&c=Index&",
+                        a: _color,
+                        sex: _sex
+                    },
+                    success: function (json) {
+                        if (json.status == 0) {
+                            var _html = "";
+                            for (var i = 0; i < json.data.length; i++) {
+                                if(json.data.lifting == 0){
+                                    json.data.lifting = "";
+                                    var _rankimgStyle = 'background: url("../img/icon_increase.jpg") 50% no-repeat; background-size: cover'
+                                }
+                                var _deNum = Math.floor(i/10);
+                                var _tempSig = ""+ i;
+                                var _sigNum = _tempSig.charAt(_tempSig.length -1);
+                                var _deStyle = 'background: url("../img/num/num_'+ _deNum +'.jpg") 50% no-repeat; background-size: cover';
+                                var _sigStyle = 'background: url("../img/num/num_'+ _sigNum +'.jpg") 50% no-repeat; background-size: cover';
 
+                                _html += '' +
+                                    '<li class="r">'+
+                                    '<div class="no"><span class="de" style="'+ _deStyle +'"></span><span class="sig" style="'+ _sigStyle +'"></span></div>'+
+                                '<span class="face" style="background: url('+ json.data[i].headimg +')"></span>'+
+                                    '<span class="name">'+ json.data[i].name +'</span>'+
+                                    '<span class="hot">热度：'+ json.data[i].votes +'</span>'+
+                                '<div class="ranking"><span class="rankIcon" style="'+ _rankimgStyle +'"></span><span>'+ json.data.lifting +'</span></div>'+
+                                '</li>'
+
+
+
+
+                            }
+                            $("#outerHtml").html(_html);
+                        } else {
+                            alert(json.msg)
+                        }
+                    }
+                });
             }
         };
         page.init()
