1ddb84fc4ee0774dac851453e810dd6d420de444
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-09-28 14:38:50 +0800

    sadf

---
+++ Application/Termin/Controller/InfoController.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace Stage\Controller;
+use Think\Controller;
+//微信端首页类
+class IndexController extends ComController {
+    //登陆 zyw916
+    public function index (){
+        
+       $this->display();
+        
+    }
+
+    
+
+}
---
+++ Public/termin/js/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by admin on 2015/9/23.
+ */
+
+define(function (require, exports, module) {
+    var nd = require("unit/nd.base");
+    require("unit/jq.$touch");
+    require("unit/jq.$pageTo");
+    $(function () {
+
+    })
+    exports.nd = nd;
+    exports.url = 'm2.nadoo.cn/p/zyw';
+});
