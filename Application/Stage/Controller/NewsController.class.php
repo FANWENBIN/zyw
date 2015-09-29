1055e680358a660a6c43af7d51ca85995144cb5c
jinyan <jinyan@nadoo.cn>
2015-09-29 15:22:36 +0800

    asdf

--- Application/Stage/Controller/NewsController.class.php
+++ Application/Stage/Controller/NewsController.class.php
@@ -6,61 +6,39 @@ class NewsController extends ComController {
     //首页显示
     public function index(){
 		$news=  M('news');
-		$result=$news->select();
+		if(isset($_POST['sousuo'])){
+			$keywords=$_POST['keywords'];
+			$map['keywords|title|content']=array('like','%'.$keywords.'%');
+		}
+		$result=$news->where($map)->select();
 		$this->assign('result',$result);
 		$this->assign('cur',5);
         $this->display();   
     }
 	public function add(){
 		$news=  M('news');
-		//var_dump($_POST);
 		$_POST['instime']=time();
-		//echo $_POST['imgpath'];
 		if($_GET['id']>0){
 			$id=$_GET['id'];
 			$data=$news->where('id ='.$id)->find();
 			$this->assign('data',$data);
 		}
 		if(isset($_POST['submit'])){
-			if(!isset($_POST['file2'])||$_POST['file2']!==$data['imgpath']){
-			//图片上传
-			$upload = new \Think\Upload();// 实例化上传类   
-			$upload->maxSize   =     3145728 ;// 设置附件上传大小   
-			$upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型  
-			$upload->savePath  =      '/news/'; // 设置附件上传目录    
-			// 上传文件   
-			$info   =   $upload->upload();    
-				if(!$info) {// 上传错误提示错误信息      
-					$this->error($upload->getError()); 
+			if($news->create()){
+				if($_POST['hid_id']>0){
+					$id=intval($_POST['hid_id']);
+					$result=$news->where('id ='.$id)->save();	
 				}else{
-					$_POST['imgpath']=$info['file']['savepath'].$info['file']['savename'];
-						if($news->create()){
-							if($_POST['hid_id']>0){
-								$id=intval($_POST['hid_id']);
-								$result=$news->where('id ='.$id)->save();
-							}
-							else{
-								$result=$news->add();
-							}
-							
-							if($result){
-								//$this->success('操作成功',U('news/index'),3);
-							}
-							else{
-								//$this->error('数据格式错误',U('news/add'),3);
-							}
-						}
-					}
-			}else{
-				if($news->create()){
-					$result=$news->where('id ='.$id)->save();
-					if($result){
-						//$this->success('操作成功',U('news/index'),3);
-					}
-					else{
-						//$this->error('数据格式错误',U('news/add'),3);
-					}
+					$result=$news->add();	
 				}
+				if($result){
+					$this->success('操作成功',U('news/index'),3);
+				}
+				else{
+					$this->error('数据格式错误',U('news/add'),3);
+				}
+			}else{
+				$this->error('数据格式错误',U('news/add'),3);
 			}
 		}
         $this->display();   
--- Public/stage/js/ueditor/php/Uploader.class.php
+++ Public/stage/js/ueditor/php/Uploader.class.php
@@ -334,11 +334,11 @@ class Uploader
      * 获取当前上传成功文件的各项信息
      * @return array
      */
-    public function getFileInfo()
+    public function getFileInfo($domain = '')
     {
         return array(
             "state" => $this->stateInfo,
-            "url" => $this->fullName,
+            "url" => $domain.$this->fullName,
             "title" => $this->fileName,
             "original" => $this->oriName,
             "type" => $this->fileType,
