4d37c3ac839dfe889aa73fd4b003dbe10814d062
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-09-29 18:12:27 +0800

    asdf

--- Application/Stage/Controller/NewsController.class.php
+++ Application/Stage/Controller/NewsController.class.php
@@ -9,9 +9,27 @@ class NewsController extends ComController {
 		if(isset($_POST['sousuo'])){
 			$keywords=$_POST['keywords'];
 			$map['keywords|title|content']=array('like','%'.$keywords.'%');
+
 		}
-		$result=$news->where($map)->select();
-		$this->assign('result',$result);
+		if(!empty($map)){
+			session('condition',$map);
+		}else{
+			session('condition','');
+		}
+
+	//	$result=$news->where($map)->select();
+		//分页显示
+		
+		$count      = $news->where(session('condition'))->count();// 查询满足要求的总记录数
+		$Page       = new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+		$show       = $Page->show();// 分页显示输出
+		// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+		$result = $news->where(session('condition'))->limit($Page->firstRow.','.$Page->listRows)->select();
+		$this->assign('result',$result);// 赋值数据集
+		$this->assign('page',$show);// 赋值分页输出
+
+
+
 		$this->assign('cur',5);
         $this->display();   
     }
