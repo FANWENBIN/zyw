56a20ce0114fc94d4a1479e3ed1b8cd0b7777669
jinyan <jinyan@nadoo.cn>
2015-09-29 20:01:03 +0800

    aaaa

--- Application/Home/Controller/NewsController.class.php
+++ Application/Home/Controller/NewsController.class.php
@@ -1,19 +1,20 @@
 <?php
-// 
 namespace Home\Controller;
 use Think\Controller;
 class NewsController extends ComController {
     public function index(){
 		$news=  M('news');
+		echo $_SERVER['DOCUMENT_ROOT'];
+		var_dump($_SERVER);
 		//形象指数
-		$resultX=$news->where('type=1')->limit('0,13')->order("instime desc")->select();
+		$resultX=$news->where('type=1 and status=1')->limit('0,13')->order("instime desc")->select();
 		//var_dump($resultX);
 		$this->assign('resultX',$resultX);
 		//形象指数end
 		
 		
 		//星闻动向
-		$resultD=$news->where('type=2')->limit('0,9')->order("instime desc")->select();
+		$resultD=$news->where('type=2 and status=1')->limit('0,9')->order("instime desc")->select();
 		//echo $news->getLastsql();
 		//var_dump($resultD);
 		$this->assign('resultD',$resultD);
@@ -21,7 +22,7 @@ class NewsController extends ComController {
 		
 		
 		//艺术中国梦
-		$resultM=$news->where('type=3')->limit('0,3')->order("instime desc")->select();
+		$resultM=$news->where('type=3 and status=1')->limit('0,3')->order("instime desc")->select();
 		$this->assign('resultM',$resultM);
 		//艺术中国梦end
 		$this->display('news');
--- Application/Stage/Controller/NewsController.class.php
+++ Application/Stage/Controller/NewsController.class.php
@@ -6,12 +6,28 @@ class NewsController extends ComController {
     //首页显示
     public function index(){
 		$news=  M('news');
+		$map['status']=1;
 		if(isset($_POST['sousuo'])){
 			$keywords=$_POST['keywords'];
 			$map['keywords|title|content']=array('like','%'.$keywords.'%');
+
 		}
-		$result=$news->where($map)->select();
-		$this->assign('result',$result);
+		if(!empty($map)){
+			session('condition',$map);
+		}else{
+			session('condition','');
+		}
+
+	//	$result=$news->where($map)->select();
+		//分页显示
+		
+		$count      = $news->where(session('condition'))->count();// 查询满足要求的总记录数
+		$Page       = new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+		$show       = $Page->show();// 分页显示输出
+		// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+		$result = $news->where(session('condition'))->limit($Page->firstRow.','.$Page->listRows)->select();
+		$this->assign('result',$result);// 赋值数据集
+		$this->assign('page',$show);// 赋值分页输出
 		$this->assign('cur',5);
         $this->display();   
     }
@@ -42,5 +58,19 @@ class NewsController extends ComController {
 			}
 		}
         $this->display();   
-    }  
+    } 
+	public function delete(){
+		$news=  M('news');
+		if($_GET['id']>0){
+			$id=$_GET['id'];
+			$map['status']=0 ;
+			$result=$news->where('id = '.$_GET['id'])->save($map);
+			if($result){
+				$this->success('操作成功',U('news/index'),3);
+			}
+			else{
+				$this->error('数据格式错误',U('news/index'),3);
+			}
+		}  
+    }	
 }
