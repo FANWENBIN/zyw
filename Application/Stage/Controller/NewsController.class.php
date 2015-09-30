c11efdb2eaf8b471ec9657afdbbb28a300b38b07
jinyan <jinyan@nadoo.cn>
2015-09-30 15:20:24 +0800

    ssss

--- Application/Stage/Controller/NewsController.class.php
+++ Application/Stage/Controller/NewsController.class.php
@@ -25,7 +25,7 @@ class NewsController extends ComController {
 		$Page       = new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数(25)
 		$show       = $Page->show();// 分页显示输出
 		// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
-		$result = $news->where(session('condition'))->limit($Page->firstRow.','.$Page->listRows)->select();
+		$result = $news->where(session('condition'))->limit($Page->firstRow.','.$Page->listRows)->order('instime desc')->select();
 		$this->assign('result',$result);// 赋值数据集
 		$this->assign('page',$show);// 赋值分页输出
 		$this->assign('cur',5);
