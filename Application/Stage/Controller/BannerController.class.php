ecce1c778fdecb3fff8d1ae2077339301d643901
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-09-30 13:10:28 +0800

    asdf

--- Application/Stage/Controller/BannerController.class.php
+++ Application/Stage/Controller/BannerController.class.php
@@ -7,6 +7,12 @@ class BannerController extends ComController {
     public function index(){
         $this->vercklogin();
         $this->assign('cur',4);
+
+
+       // $image = new \Think\Image(); $image->open('./1.jpg');
+        // 按照原图的比例生成一个最大为150*150的缩略图并保存为thumb.jpg   1200  470      
+       / $image->thumb(120, 47,\Think\Image::IMAGE_THUMB_SCALE)->save('./thumb.jpg');
+
         $this->display();
     }
 }
