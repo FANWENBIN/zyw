793284264d2d4ef8e111a931510f95b432afe752
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-09 16:30:00 +0800

    asdf

--- Application/Home/Controller/NewsController.class.php
+++ Application/Home/Controller/NewsController.class.php
@@ -95,6 +95,21 @@ class NewsController extends ComController {
 	 }
 	 //星闻动向
 	 public function news_star(){
+	 	$news = M('news');
+		$count      = $news->where('type=2')->order('instime desc')->count();// 查询满足要求的总记录数
+		$Page       = new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+		$show       = $Page->show();// 分页显示输出
+		// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+		$list = $news->where('type=2')->order('instime desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+		$this->assign('list',$list);// 赋值数据集
+		$this->assign('page',$show);// 赋值分页输出
+		//时间顺序
+		$timenews = $news->where('type=2')->order('instime desc')->limit(0,7)->select();
+		$this->assign('timenews',$timenews);
+		//热门新闻
+		$hotnews = $news->where('type=2')->order('hot desc')->limit('0,10')->select();
+		$this->assign('hotnews',$hotnews);
+
 	 	$this->display();
 	 }
 }
