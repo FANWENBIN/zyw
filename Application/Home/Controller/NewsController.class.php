917de03acd6b07d67c970803f3cc77d96f8cd498
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-09 15:57:35 +0800

    asdf

--- Application/Home/Controller/NewsController.class.php
+++ Application/Home/Controller/NewsController.class.php
@@ -64,7 +64,7 @@ class NewsController extends ComController {
 	 	$news = M('news');
 	 	//$newsval = $news->where('type=1')->order('instime desc')->select();
 		$count      = $news->where('type=1')->order('instime desc')->count();// 查询满足要求的总记录数
-		$Page       = new \Think\Page($count,2);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+		$Page       = new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数(25)
 		$show       = $Page->show();// 分页显示输出
 		// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
 		$list = $news->where('type=1')->order('instime desc')->limit($Page->firstRow.','.$Page->listRows)->select();
