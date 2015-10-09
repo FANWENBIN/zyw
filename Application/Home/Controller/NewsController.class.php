f1ea3e65569a54e219e244cfe565f56572548db3
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-09 16:25:05 +0800

    asdf

--- Application/Home/Controller/NewsController.class.php
+++ Application/Home/Controller/NewsController.class.php
@@ -56,6 +56,20 @@ class NewsController extends ComController {
 	 }
 	 //艺术中国梦
 	 public function news_dream(){
+	 	$news = M('news');
+		$count      = $news->where('type=3')->order('instime desc')->count();// 查询满足要求的总记录数
+		$Page       = new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+		$show       = $Page->show();// 分页显示输出
+		// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+		$list = $news->where('type=3')->order('instime desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+		$this->assign('list',$list);// 赋值数据集
+		$this->assign('page',$show);// 赋值分页输出
+		//时间顺序
+		$timenews = $news->where('type=3')->order('instime desc')->limit(0,7)->select();
+		$this->assign('timenews',$timenews);
+		//热门新闻
+		$hotnews = $news->where('type=3')->order('hot desc')->limit('0,10')->select();
+		$this->assign('hotnews',$hotnews);
 
 	 	$this->display();
 	 }
@@ -68,10 +82,11 @@ class NewsController extends ComController {
 		$show       = $Page->show();// 分页显示输出
 		// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
 		$list = $news->where('type=1')->order('instime desc')->limit($Page->firstRow.','.$Page->listRows)->select();
-		echo $news->getlastsql();
 		$this->assign('list',$list);// 赋值数据集
 		$this->assign('page',$show);// 赋值分页输出
-		
+		//时间顺序
+		$timenews = $news->where('type=1')->order('instime desc')->limit(0,7)->select();
+		$this->assign('timenews',$timenews);
 		//热门新闻
 		$hotnews = $news->where('type=1')->order('hot desc')->limit('0,10')->select();
 		$this->assign('hotnews',$hotnews);
