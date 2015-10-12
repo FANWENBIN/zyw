97427579b91f0084a896ece8ea260e39a5db93ae
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-12 15:39:23 +0800

    asdf

---
+++ Application/Stage/Controller/FuturestarController.class.php
@@ -0,0 +1,17 @@
+<?php
+// 
+namespace Stage\Controller;
+use Think\Controller;
+class FuturestarController extends ComController {
+	//明日之星首页显示
+	public function index(){
+		$committee  = M('committee');
+		$commitval = $committee->where('status = 1')->select();
+		$this->assign('commitval',$commitval);
+		$this->assign('cur',7);
+		$this->display();
+		//echo $ip = get_client_ip();
+	}
+	
+
+}
