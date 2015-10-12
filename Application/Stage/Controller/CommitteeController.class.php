ea277d3b77422341577e4d324274ba2b8270a3d6
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-12 14:15:25 +0800

    ;asdf

--- Application/Stage/Controller/CommitteeController.class.php
+++ Application/Stage/Controller/CommitteeController.class.php
@@ -4,11 +4,16 @@ namespace Stage\Controller;
 use Think\Controller;
 class CommitteeController extends ComController {
 	//首页显示
-    public function index(){
-    	$this->assign('cur',7);
+	public function index(){
+		$committee  = M('committee');
+		$commitvval = $committee->where('status = 1')->select();
+		$this->assign('commitval',$commitval);
+		$this->assign('cur',7);
 		$this->display();
 		//echo $ip = get_client_ip();
-    }
-   
+	}
+	public function add(){
+		
+	}
 
 }
