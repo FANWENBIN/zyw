7367ac97ad3ef50a38ba680a83504275ddbe6115
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-12 14:16:17 +0800

    ;asdf

--- Application/Stage/Controller/CommitteeController.class.php
+++ Application/Stage/Controller/CommitteeController.class.php
@@ -6,14 +6,15 @@ class CommitteeController extends ComController {
 	//首页显示
 	public function index(){
 		$committee  = M('committee');
-		$commitvval = $committee->where('status = 1')->select();
+		$commitval = $committee->where('status = 1')->select();
 		$this->assign('commitval',$commitval);
+		var_dump($commitval)
 		$this->assign('cur',7);
 		$this->display();
 		//echo $ip = get_client_ip();
 	}
 	public function add(){
-		
+
 	}
 
 }
