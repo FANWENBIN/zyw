164bea7266e919bc56507190111d4277b1c32eb4
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-12 14:18:42 +0800

    ;asdf

--- Application/Stage/Controller/CommitteeController.class.php
+++ Application/Stage/Controller/CommitteeController.class.php
@@ -8,13 +8,19 @@ class CommitteeController extends ComController {
 		$committee  = M('committee');
 		$commitval = $committee->where('status = 1')->select();
 		$this->assign('commitval',$commitval);
-		var_dump($commitval);
 		$this->assign('cur',7);
 		$this->display();
 		//echo $ip = get_client_ip();
 	}
 	public function add(){
+		$submit = I('post.submit');
+		if(empty($submit)){
 
+			$this->display();
+		}else{
+			
+		}
+		
 	}
 
 }
