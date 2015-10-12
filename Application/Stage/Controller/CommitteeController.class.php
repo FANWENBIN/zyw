0bc9204fb4d1c8cf2b6c356277aa541491f025b6
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-12 13:43:04 +0800

    ;asdf

--- Application/Stage/Controller/CommitteeController.class.php
+++ Application/Stage/Controller/CommitteeController.class.php
@@ -5,7 +5,7 @@ use Think\Controller;
 class CommitteeController extends ComController {
 	//首页显示
     public function index(){
-
+    	$this->assign('cur',7);
 		$this->display();
 		//echo $ip = get_client_ip();
     }
