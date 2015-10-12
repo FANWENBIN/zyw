131e2a15d254befc100f71510861106723fbc34c
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-12 16:28:36 +0800

    asdf

--- Application/Home/Controller/CommitteeController.class.php
+++ Application/Home/Controller/CommitteeController.class.php
@@ -6,8 +6,31 @@ class CommitteeController extends ComController {
 	//首页显示
     public function index(){
     	$committee = M('committee');
-    	$list = $committee->where('status = 1')->order('top desc,instime desc')->select();
+    	//推荐置顶之声
+    	$list = $committee
+    			->where('status = 1 and top = 1')
+    			->order('top desc,instime desc')
+    			->select();
     	$this->assign('list',$list);
+    	//形象监督
+    		//红榜
+    	$count = $committee->where('status=1 and type=1')->count();// 查询满足要求的总记录数
+    	$Page  = new \Think\Page($count,9);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+    	$show  = $Page->show();// 分页显示输出
+    	// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+    	$list = $committee->where('status=1 and type=1')->order('create_time')->limit($Page->firstRow.','.$Page->listRows)->select();
+    	$this->assign('redcom',$list);// 赋值数据集
+    	$this->assign('redpage',$show);// 赋值分页输出
+
+    		//黑榜
+    	$count = $committee->where('status=1 and type=2')->count();// 查询满足要求的总记录数
+    	$Page  = new \Think\Page($count,9);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+    	$show  = $Page->show();// 分页显示输出
+    	// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+    	$list = $committee->where('status=1 and type=2')->order('create_time')->limit($Page->firstRow.','.$Page->listRows)->select();
+    	$this->assign('blackcom',$list);// 赋值数据集
+    	$this->assign('blackpage',$show);// 赋值分页输出
+
 		$this->display();
 		//echo $ip = get_client_ip();
     }
