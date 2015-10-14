8b81f0ed751371d15119e929b51ef021f42cb770
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-14 19:20:19 +0800

    adsf

--- Application/Stage/Controller/VedioController.class.php
+++ Application/Stage/Controller/VedioController.class.php
@@ -22,21 +22,73 @@ class VedioController extends ComController {
     		$vedioval = $vedio->where('id='.$id)->find();
     		$this->assign('vedioval',$vedioval);
     		$this->display();
+    	}else{
+    		$id = I('get.id');
+    		$data['title'] = I('post.title');
+    		$data['type']  = I('post.type');
+    		$data['href']  = I('post.href');
+    		$data['img']   = I('post.img');
+    		$sign = $this->checkDump($data);
+    		if(!$sign){
+    			$this->error('主要信息不可为空');
+    		}
+			$data['recommend'] = I('post.recommend');
+			if($data['recommend'] == 1){
+				$data['topimg'] = I('post.topimg');
+			}
+    		$sign = $vedio->where('id='.$id)->save($data);
+    		if($sign){
+    			$this->success('修改成功',U('Vedio/index'));
+    		}else{
+    			$this->error('未做任何修改');
+    		}
+    		
+
     	}
     }
     //删除视频
     public function delete(){
-    	$id = I('get.id');
+    	$id = I('post.id','','intval');
     	$vedio = M('vedio');
-    	$data['status'] = 1;
+    	$data['status'] = 0;
 
     	$sign = $vedio->where('id='.$id)->save($data);
+   
     	if($sign){
     		$this->success('删除成功',U('Vedio/index'));
     	}else{
-    		$this->error('未删除数据');
+    		$this->error('未删除');
     	}
+    }
+    //新增数据
+    public function add(){
+    	$submit = I('post.submit');
+    	$vedio  = M('vedio');
+    	if(empty($submit)){
+    		$this->assign('cur',9);
+    		$this->display();
+    	}else{
+    		$data['title'] = I('post.title');
+    		$data['type']  = I('post.type');
+    		$data['href']  = I('post.href');
+    		$data['img']   = I('post.img');
+    		$sign = $this->checkDump($data);
+    		if(!$sign){
+    			$this->error('主要信息不可为空');
+    		}
+			$data['recommend'] = I('post.recommend');
+			if($data['recommend'] == 1){
+				$data['topimg'] = I('post.topimg');
+			}
+			$data['instime'] = time();
 
+    		$sign = $vedio->add($data);
+    		if($sign){
+    			$this->success('新增成功',U('Vedio/index'));
+    		}else{
+    			$this->error('未做任何新增');
+    		}
+    	}
 
     }
 }
