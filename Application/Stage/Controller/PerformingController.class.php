2ab3b0ab32543e91928a3bdb3534ff84a7808b75
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-15 18:02:33 +0800

    asdf

--- Application/Stage/Controller/PerformingController.class.php
+++ Application/Stage/Controller/PerformingController.class.php
@@ -199,6 +199,23 @@ class PerformingController extends ComController {
             $this->error('未删除');
         }
     }
+    //待审核列表
+    public function audit(){
+        $actors = M('actors');
 
+        //分页
+        $count = $actors->order('votes desc')->where('status=3')->count();// 查询满足要求的总记录数
+        $Page  = new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+        $show  = $Page->show();// 分页显示输出// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+        $actorsval  = $actors->order('votes desc')->where('status=2')
+                ->limit($Page->firstRow.','.$Page->listRows)->select();
+
+        $this->assign('actors',$actorsval);// 赋值数据集
+        $this->assign('page',$show);// 赋值分页输出
+
+        $this->assign('cur',10);
+        $this->display();
+        //echo md5('xxxzyw916');
+    }
 
 }
