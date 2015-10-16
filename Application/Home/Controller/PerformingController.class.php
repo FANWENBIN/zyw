9807d2e5f7a136498f4a1f9417a311f520a23d94
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-16 15:03:05 +0800

    asdf

--- Application/Home/Controller/PerformingController.class.php
+++ Application/Home/Controller/PerformingController.class.php
@@ -6,6 +6,47 @@ class PerformingController extends ComController {
     public function index(){
         $this->display();
     }
+    //演艺人全部按照字母排序接口数据
+    public function allperforming(){
+    	$type = trim(I('get.type'));
+    	switch ($type) {
+    		case '1':       //男演员
+    			$where['sex'] = 1;
+    			break;
+    		case '2':		//女演员
+    			$where['sex'] = 2;
+    			break;
+    		case '3':		//港台演员
+    			$where['area'] = 1;
+    			break;
+    		case '4':		//内地演员
+    			$where['area'] = 2;
+    			break;
+    		case '5':		//演艺新人
+    			$where['newser'] = 1;
+    			break;	
+    		default:
+
+    			break;
+    	}
+    	$actors = M('actors');
+    	$data = [];
+
+    	foreach(range('A','Z') as $v){
+			$data[$v] = $actors
+						->field('id,headimg,name')
+						->where("status <> 0 and initial ='".$v."'")
+						->where($where)
+						->select();
+			foreach($data[$v] as $key=>$val){
+				$data[$v][$key]['headimg'] = './Uploads'.$val['headimg'];
+			}
+			if($data[$v] === false){
+				ajaxReturn(101,'系统错误','');
+			}
+		}
+		ajaxReturn(0,'',$data);
+    }
 
 
 }
