e5d42ecf421222becb970dd21a9327089f1e354a
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-19 15:36:39 +0800

    asdf

--- Application/Home/Controller/StageController.class.php
+++ Application/Home/Controller/StageController.class.php
@@ -4,8 +4,14 @@ namespace Home\Controller;
 use Think\Controller;
 class StageController extends ComController {
     public function index(){
+    	$actors = M('actors');
+    	$list = $actors->where('newser = 1')->order('clickrate desc')->limit(0,18)->select();
+    	$this->assign('list',$list);
 		$this->display();
     }
-   
+    //学员报名
+   	public function apply(){
+
+   	}
    
 }
