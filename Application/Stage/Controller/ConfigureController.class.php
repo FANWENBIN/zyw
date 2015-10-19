9003016ff411c4dc8d69b000b62e4fed6069c05c
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-19 17:35:30 +0800

    asdf

---
+++ Application/Stage/Controller/ConfigureController.class.php
@@ -0,0 +1,66 @@
+<?php
+namespace Stage\Controller;
+use Think\Controller;
+//演艺库类
+class ConfigureController extends ComController {
+    //备案号设置
+    public function index(){
+        $submit = I('post.submit');
+        $configure = M('configure');
+        if(empty($submit)){
+            $list = $configure->where('type = 4')->find();
+            $this->assign('list',$list);
+            $this->assign('cur',12);
+            $this->display();  
+        }else{
+            $data['records'] = I('post.records');
+            $sign = $configure->where('type = 4')->save($data);
+            if($sign){
+                $this->success('保存成功',U('Configure/index'));
+            }else{
+                $this->error('保存失败');
+            }
+        }  
+          
+    }
+    //logo设置
+    public function logo(){
+        $submit = I('post.submit');
+        $configure = M('configure');
+        if(empty($submit)){
+            $list = $configure->where('type = 4')->find();
+            $this->assign('list',$list);
+            $this->assign('cur',12);
+            $this->display();
+        }else{
+            $data['records'] = I('post.records');
+            $sign = $configure->where('type = 4')->save($data);
+            if($sign){
+                $this->success('保存成功',U('Configure/index'));
+            }else{
+                $this->error('保存失败');
+            }
+        }  
+    }
+    //报名规则
+    public function rule(){
+        $submit = I('post.submit');
+        $configure = M('configure');
+        if(empty($submit)){
+            $list = $configure->where('type = 1')->find();
+            $this->assign('list',$list);
+            $this->assign('cur',12);
+            $this->display();
+        }else{
+            $data['stagetitle'] = I('post.stagetitle');
+            $data['stagerule'] = I('post.stagerule');
+            $sign = $configure->where('type = 1')->save($data);
+            if($sign){
+                $this->success('保存成功',U('Configure/index'));
+            }else{
+                $this->error('保存失败');
+            }
+        }  
+    }
+
+}
