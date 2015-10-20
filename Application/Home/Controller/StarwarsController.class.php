eb290f488648f4cc7a8c7d13649bcdb61a538d1b
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-20 15:47:48 +0800

    asdf

--- Application/Home/Controller/StarwarsController.class.php
+++ Application/Home/Controller/StarwarsController.class.php
@@ -6,4 +6,14 @@ class StarwarsController extends ComController {
     public function index(){
         $this->display();
     }
+    public function starinfo(){
+    	$id = I('get.id');
+    	$recommend = M('recommend');
+    	$recommendval = $recommend->where()->find();
+    	$this->assign('list',$recommendval);
+    	$recommend_production = M('recommend_production');
+    	$production = $recommend_production->where('recommendid='.$id)->select();
+    	$this->assign('production',$production);
+    	$this->display();
+    }
 }
