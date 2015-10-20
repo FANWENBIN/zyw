5e55f8c5cc6434b6f10e7416db6f73d4fd0b161a
yangronghai <ronghai.yang@nadoo.cn>
2015-10-20 12:39:24 +0800

    update active.html ajax test

--- Public/statics/src/js/active/active.js
+++ Public/statics/src/js/active/active.js
@@ -3,7 +3,8 @@ $(function(){
         now: 0,
         timer: null,
         type: 0,
-        time: 0
+        time: 0,
+        pageNum: 1
     };
     var page = {
         init: function(){
@@ -14,10 +15,15 @@ $(function(){
             $("#imgList").on("mouseout",page.mouseout);
             $("#newActive").on("click",page.createActive);
             page.autoTab();
-            page.getActiveData(1);
-            pageinit(5,function(index){
-              console.log(index);
+
+            //分页
+            page.getPage(1，function(){
+              pageinit(scope.pageNum,function(index){
+                page.getActiveData(index);
+              });
             });
+
+
         },
         tabBanner: function(){
             clearTimeout(scope.timer);
@@ -63,6 +69,46 @@ $(function(){
         createActive: function(){
             $("#mask").show();
         },
+
+        /*
+        <li>
+            <a>
+                <img src="__PUBLIC__/statics/images/p_active.jpg">
+                <div class="tags">
+                    <h3>伟来眼中的他们</h3>
+                    <p>10.25-11.2</p>
+                </div>
+                <sub></sub>
+            </a>
+            <div class="desc">
+                <span class="txt">伟来眼中的他们伟来眼中的他们伟来眼中的他们伟来眼中的他们伟来眼中的他们伟来眼中的他们伟来眼中的他们伟来眼中的他们伟来眼中的他们伟来眼中的他们伟来眼中的他们伟来眼中的他们伟来眼中的他们</span>
+                <div><span class="num">12313</span><i class="focus"></i></div>
+            </div>
+        </li>
+
+        */
+
+        getPage: function(fn){
+          $.ajax({
+            url: "./index.php?m=Home&c=Active&a=activetype",
+            type: "get",
+            dateType: "json",
+            data: {
+              type: scope.type,
+              time: scope.time,
+              p: 1
+            },
+            success: function(json){
+              console.log(json);
+              scope.pageNum = json.data.page;
+              fn();
+            },
+            error: function(){
+
+            }
+
+          })
+        },
         getActiveData: function(page){
           $.ajax({
             url: "./index.php?m=Home&c=Active&a=activetype",
@@ -75,6 +121,7 @@ $(function(){
             },
             success: function(json){
               console.log(json);
+              scope.pageNum = json.data.page;
             },
             error: function(){
 
