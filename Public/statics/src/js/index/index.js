afffc087a94fb6e4a32d61374da22f3af576b13c
yangronghai <ronghai.yang@nadoo.cn>
2015-10-22 16:01:34 +0800

    update index.html tab effect

--- Public/statics/src/js/index/index.js
+++ Public/statics/src/js/index/index.js
@@ -5,6 +5,8 @@ $(function() {
     init: function() {
 
       $("#J_conTopStars .select a").click(page.clickTopStars);
+			$("#eventlist").on("click","li",page.tabEvent);
+			$("#videolist").on("click","li",page.tabVideo);
     },
 
     clickTopStars: function() {
@@ -12,7 +14,19 @@ $(function() {
       $(this).addClass("c");
       $(this).parents(".group").find(".list").hide();
       $(this).parents(".group").find(".list").eq($(this).index()).show()
-    }
+    },
+		tabEvent: function(){
+			$("#eventlist").find("li").removeClass("c");
+			$(this).addClass("c");
+			$("#eventgroup").find("li").removeClass("active");
+			$("#eventgroup").find("li").eq($(this).index()).addClass("active");
+		},
+		tabVideo: function(){
+			$("#videolist").find("li").removeClass("c");
+			$(this).addClass("c");
+			$("#videogroup").find(".list").removeClass("active");
+			$("#videogroup").find(".list").eq($(this).index()).addClass("active");
+		}
   };
   page.init();
 });
