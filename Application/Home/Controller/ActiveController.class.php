2f67e6fc1ce8c90f2279e1ca8308d84de4578e34
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-23 19:11:28 +0800

    asdf

--- Application/Home/Controller/ActiveController.class.php
+++ Application/Home/Controller/ActiveController.class.php
@@ -87,7 +87,7 @@ class ActiveController extends ComController {
         $Page       = new \Think\Page($count,12);// 实例化分页类 传入总记录数和每页显示的记录数(25)
         $show       = $Page->show();// 分页显示输出
         // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
-$list = $active->where($data)->order('instime')->limit($Page->firstRow.','.$Page->listRows)->select();
+        $list = $active->where($data)->order('instime')->limit($Page->firstRow.','.$Page->listRows)->select();
     //$this->assign('list',$list);// 赋值数据集
    // $this->assign('page',$show);// 赋值分页输出
 
@@ -97,6 +97,7 @@ $list = $active->where($data)->order('instime')->limit($Page->firstRow.','.$Page
     		foreach($list as $key=>$val){
     			$list[$key]['begin_time'] = date('m.d',$val['begin_time']);
     			$list[$key]['last_time'] = date('m.d',$val['last_time']);
+                 echo strlen($val['title'])>80?mb_substr($val['title'],0,80,'utf-8').'......':$val['title'];
     		}
             $dump['page'] = ceil($count/12);
             $dump['data'] = $list;
