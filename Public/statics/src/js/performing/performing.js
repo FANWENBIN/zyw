4cdb1b35366d44c280ea7d39c24f1f1a05cb7564
yangronghai <ronghai.yang@nadoo.cn>
2015-10-23 16:37:41 +0800

    update performing

--- Public/statics/src/js/performing/performing.js
+++ Public/statics/src/js/performing/performing.js
@@ -1,6 +1,6 @@
 $(function(){
   var scope = {
-
+    group: 0
   };
   var page = {
     init: function(){
@@ -10,7 +10,72 @@ $(function(){
       $("#sorting").find("li").removeClass("active");
       $(this).addClass("active");
       //回调 刷新列表
-      console.log($(this).data("group"));
+      scope.group = $(this).data("group");
+      page.loadData();
+    },
+    loadData: function(){
+      $.ajax({
+        url: "./index.php?m=Home&c=Performing&a=allperforming",
+        type: "get",
+        dataType: "json",
+        data: {
+          type: scope.group
+        },
+        success: function(json){
+          /*
+
+          <li class="alphabet-list">
+            <div class="title">
+              <h3 id="a">A</h3>
+            </div>
+            <div class="inner clearFix">
+              <a href = "#">
+                <div class="item">
+                  <img src="__PUBLIC__/statics/images/p_starwars_1.jpg" alt="" />
+                  <p>angularbaby</p>
+                  <sub></sub>
+                </div>
+              </a>
+
+            </div>
+          </li>
+          */
+
+          if(json.status == 0){
+            var _outerhtml = '';
+            for(key in item){
+              var _innerhtml = '<li class="alphabet-list"><div class="title"><h3 id="a">'+ key +'</h3></div>'
+                +'<div class="inner clearFix">'
+              ;
+              //item[key]
+              if(item[key]){
+                for(var i = 0, len = item[key].length; i < len ; i++){
+                  //item[key][i].
+                  _innerhtml += '<a href = "#'+ item[key][i].id +'">'
+                      +'<div class="item">'
+                      +'<img src="'+ item[key][i].headimg +'" alt="" />'
+                      +'<p>'+ item[key][i].name +'</p>'
+                      +'<sub></sub>'
+                    +'</div>'
+                  +'</a>'
+                  +'</div>'
+                  +'</li>'
+
+                }
+              }else{
+                _innerhtml += '</div></li>'
+              }
+            _outerhtml += _innerhtml;
+            }
+            $("#actorgroup").html(_outerhtml);
+          }else{
+          }
+        },
+        error: function(){
+
+        }
+
+      })
     }
   };
   page.init();
