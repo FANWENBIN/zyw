24d85915b084fb21f4079afba971e8483d083005
yangronghai <ronghai.yang@nadoo.cn>
2015-10-24 14:11:50 +0800

    update all.html

--- Public/statics/src/js/newstar/newstar.js
+++ Public/statics/src/js/newstar/newstar.js
@@ -1,1 +1,33 @@
- 
+$(function() {
+  var scope = {
+
+  };
+  var page = {
+    init: function() {
+      tabinit();
+      $("#hot").on("click",page.showHot);
+      $("#new").on("click",page.showNew);
+      $("#hotcouse").on("click","li",page.tabHotCouse)
+    },
+    showHot: function(){
+      $(this).addClass("active");
+      $("#hotlist").addClass("active");
+      $("#new").removeClass("active");
+      $("#newlist").removeClass("active");
+    },
+    showNew: function(){
+      $(this).addClass("active");
+      $("#newlist").addClass("active");
+      $("#hot").removeClass("active");
+      $("#hotlist").removeClass("active");
+    },
+    tabHotCouse: function(){
+      $("#hotcouse").find("li").removeClass("active");
+      $(this).addClass("active");
+      $("#hotcousegroup").find("li").removeClass("active");
+      $("#hotcousegroup").find("li").eq($(this).index()).addClass("active");
+    }
+
+  };
+  page.init();
+})
