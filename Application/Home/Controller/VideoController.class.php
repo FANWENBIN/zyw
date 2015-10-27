c3247e12cc72f422893717f0b36f2f4e796ea636
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-27 11:38:43 +0800

    asdf

--- Application/Home/Controller/VideoController.class.php
+++ Application/Home/Controller/VideoController.class.php
@@ -47,4 +47,33 @@ class VideoController extends ComController {
   public function video_more(){
     $this->display();
   }
+  public function vial(){
+    $type = intval(trim(I('get.type')));
+    $vedio = M('vedio');
+    //视频汇
+    $data['type'] = $type;
+    //$tvvideo = $vedio->where($data)->order('instime desc,hot desc')->limit(0,15)->select();
+
+    $count      = $vedio->where($data)->count();
+    // 查询满足要求的总记录数
+    $Page       = new \Think\Page($count,15);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+    $show       = $Page->show();// 分页显示输出
+    // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+    $list = $vedio->field('id,instime,bigimg,title')->where($data)->order('instime desc,hot desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+
+    $dump['page'] = ceil($count/15);
+    $dump['data'] = $list;
+    if($list === false){
+        ajaxReturn(101,'请求失败','');
+    }else{
+         if(!$dump['data']){
+            $dump['data'] = array();
+        }
+        foreach($list as $key=>$val){
+            $dump['data'][$key]['instime'] = date('m-d',$val['instime']);
+            $dump['data'][$key]['title'] = mb_strlen($val['title'],'utf8')>12?mb_substr($val['title'], 0,12,'utf8').'...':$val['title'];
+        }
+        ajaxReturn(0,'',$dump);
+    }
+  }
 }
