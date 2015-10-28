099b3780b222a9dd0d5d4949b41ec9ad331ed91a
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-10-28 12:29:59 +0800

    asdf

--- Application/Home/Controller/CommitteeController.class.php
+++ Application/Home/Controller/CommitteeController.class.php
@@ -43,7 +43,7 @@ class CommitteeController extends ComController {
     	$Page  = new \Think\Page($count,12);// 实例化分页类 传入总记录数和每页显示的记录数(25)
     	$show  = $Page->show();// 分页显示输出
     	// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
-    	$list = $committee->field('img,title,instime')->where('status=1 and type=1')->order('instime')->limit($Page->firstRow.','.$Page->listRows)->select();
+    	$list = $committee->field('id,img,title,instime')->where('status=1 and type=1')->order('instime')->limit($Page->firstRow.','.$Page->listRows)->select();
     	foreach($list as $key=>$val){
     		$list[$key]['instime'] = date('Y-m-d H:i:s',$val['instime']);
     	}
@@ -67,7 +67,7 @@ class CommitteeController extends ComController {
     	$Page  = new \Think\Page($count,12);// 实例化分页类 传入总记录数和每页显示的记录数(25)
     	$show  = $Page->show();// 分页显示输出
     	// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
-    	$list = $committee->field('img,title,instime')->where('status=1 and type=2')->order('instime')->limit($Page->firstRow.','.$Page->listRows)->select();
+    	$list = $committee->field('id,img,title,instime')->where('status=1 and type=2')->order('instime')->limit($Page->firstRow.','.$Page->listRows)->select();
     	foreach($list as $key=>$val){
     		$list[$key]['instime'] = date('Y-m-d H:i:s',$val['instime']);
     	}
@@ -85,6 +85,18 @@ class CommitteeController extends ComController {
     //演工委之声详情
     public function committee_details(){
         $id = I('get.id');
+
+
+
+$news=  M('news');
+         //热点
+         $hotnews=$news->limit('0,5')->order(array('order'=>'desc','instime'=>'desc'))->select();
+         $this->assign('hotnews',$hotnews);
+         //活动
+         $active = M('active');
+         $activeval = $active->where('status = 1')->order('`order` desc,instime desc,concern desc')->find();
+
+         $this->assign('activeval',$activeval);
         $this->display();
     }
 }
