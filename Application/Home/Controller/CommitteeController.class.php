f2fdafa0b8903885d514c5a64d89b23c8e913a79
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-11-02 18:48:53 +0800

    sadf

--- Application/Home/Controller/CommitteeController.class.php
+++ Application/Home/Controller/CommitteeController.class.php
@@ -53,11 +53,11 @@ class CommitteeController extends ComController {
     		//红榜
     public function redcom(){
     	$committee = M('committee');
-    	$count = $committee->where('status=1 and type=1')->count();// 查询满足要求的总记录数
+    	$count = $committee->where('status=1')->count();// 查询满足要求的总记录数
     	$Page  = new \Think\Page($count,12);// 实例化分页类 传入总记录数和每页显示的记录数(25)
     	$show  = $Page->show();// 分页显示输出
     	// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
-    	$list = $committee->field('id,img,title,instime')->where('status=1 and type=1')->order('instime')->limit($Page->firstRow.','.$Page->listRows)->select();
+    	$list = $committee->field('id,img,title,instime')->where('status=1')->order('instime')->limit($Page->firstRow.','.$Page->listRows)->select();
     	foreach($list as $key=>$val){
     		$list[$key]['instime'] = date('Y-m-d H:i:s',$val['instime']);
     	}
