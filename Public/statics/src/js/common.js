3d1f1f679c3494255456e485c1aa66bcdeac30af
yangronghai <ronghai.yang@nadoo.cn>
2015-11-03 20:15:24 +0800

    verify

--- Public/statics/src/js/common.js
+++ Public/statics/src/js/common.js
@@ -29,7 +29,7 @@ $(function() {
       }
     },
     getVer: function() {
-      console.log($(":text[name=mb]").val(), $(":text[name=idcode1]").val())
+      console.log(typeof $(":text[name=mb]").val(),typeof $(":text[name=idcode1]").val())
       $.ajax({
         url: './index.php?m=Home&c=Login&a=yzm',
         type: 'get',
