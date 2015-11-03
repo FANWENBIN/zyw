7f4d6a8eee919c66e26978f61658a4b9a901e895
yangronghai <ronghai.yang@nadoo.cn>
2015-11-03 19:35:59 +0800

    update verify

--- Public/statics/src/js/common.js
+++ Public/statics/src/js/common.js
@@ -5,17 +5,53 @@ $(function() {
   };
   var page = {
     init: function(){
+      // 点击#login 显示登陆框
       $("#login").on("click",page.showInfo);
+      // 点击body隐藏登录框
       $("body").on("click",page.hideInfo);
+      // 未登陆时，点击#reg 注册框出来
+      $("#reg").on("click",page.regShow);
+      // 未登陆时，点击#log 登录框出来
+      $("#log").on("click",page.logShow);
+      // 上面两个框的关闭按钮
+      $(".close").on("click",page.closeAlert);
+      // 刷新二维码
+      $("#img1").on("click",page.changePic);
+      $("#img2").on("click",page.changePic);
+      $("#getfreemesg").on("click",page.getVer)
+      // 增加$.testLogin函数验证登陆
       page.addLogin();
+      // 用户登陆检测判断
       if($.testLogin()){
         $("#islogin").show();
       }else{
         $("#nologin").show();
       }
-      $("#reg").on("click",page.regShow);
-      $("#log").on("click",page.logShow);
-      $(".close").on("click",page.closeAlert)
+    },
+    getVer: function(){
+      $.ajax({
+        url: "./index.php?m=Home&c=Login&a=yzw",
+        type: "get",
+        dataType: "json",
+        data: {
+          phone: $(":text[name=mb]"),
+          code: $(":text[name=idcode1]")
+        }
+        success: function(json){
+          if(json.status == "102"){
+            $("#error").html("验证码输入错误");
+          }else if(json.status == "101"){
+            $("#error").html("服务器错误，请稍后再试")
+          }elese if(json.status == "0"){
+            $("#error").html("手机验证码获取成功")
+          }
+        },
+        error: function(){
+        }
+      })
+    },
+    changePic: function(){
+      $("#img1").attr("src","./index.php?m=Home&c=Login&a=verify");
     },
     closeAlert: function(){
       $(this).parent().parent().hide();
@@ -33,6 +69,7 @@ $(function() {
     hideInfo: function(){
         $("#myinfoalert").hide();
     },
+    // jq插件 增加testLogin函数验证登陆
     addLogin: function(){
       $.extend({
         testLogin: function(){
@@ -45,7 +82,6 @@ $(function() {
               if(json.status == "1")return true;
             },
             error: function(){
-
             }
           })
         }
