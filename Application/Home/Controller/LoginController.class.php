c2dc9286bee1fcf3cd0f76fdf6a00f164a3c8973
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-11-03 19:27:36 +0800

    adsf

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -104,10 +104,11 @@ class LoginController extends Controller {
         //随机生成验证码
         $ver = rand(1000,9999);
         $phone = I('get.phone');
-        if(!preg_match("/^13[0-9]{1}[0-9]{8}$|15[0189]{1}[0-9]{8}$|189[0-9]{8}$/",$phone)){    
-            ajaxReturn(103,'手机输入不符合格式');   
-         
-            }
+
+        if(!preg_match("/1[3458]{1}\d{9}$/",$phone)){  
+            ajaxReturn(103,'手机输入不符合格式');  
+        }
+
         $sign = $this->sms($phone,$ver);
         if($sign){
             ajaxReturn(101,'发送失败','');
