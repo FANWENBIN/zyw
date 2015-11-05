792f309c8e0dd6a96f76e5f2852a58256830eccd
yangronghai <ronghai.yang@nadoo.cn>
2015-11-05 16:57:25 +0800

    update setting

--- Public/statics/src/js/user/mysetting.js
+++ Public/statics/src/js/user/mysetting.js
@@ -1,5 +1,6 @@
 $(function(){
   var scope = {
+    provinceid: "4000"
 
   };
   var page = {
@@ -10,13 +11,47 @@ $(function(){
       $("#changeNum").on("click",page.toStep2);
       $("#sendverify").on("click",page.getVerInSetting);
       $("#confirmmb").on("click",page.confirmMb);
+      // 初始化province city
+      page.getProvince();
+      page.getCity();
 
     },
     getProvince: function(){
-      
+      $.ajax({
+        url: "./index.php?m=Home&c=Area&a=province",
+        type: "get",
+        dataType: "json",
+        success: function(json){
+
+          var _arr = json.data.data;
+          var _html = "";
+          for(var i = 0, len = _arr.length ; i < len ; i++){
+            _html += '<option data-id="'+ _arr[i].provinceid +'">'+ _arr[i].province +'</option>'
+          }
+          $("#province").html(_html);
+        },
+        error: function(){}
+      })
     },
     getCity: function(){
+      $.ajax({
+        url: "./index.php?m=Home&c=Area&a=city",
+        type: "get",
+        dataType: "json",
+        data: {
+          provinceid: scope.provinceid
+        },
+        success: function(json){
+          var _arr = json.data.data;
+          var _html = "";
+          for(var i = 0, len = _arr.length ; i < len ; i++){
+            _html += '<option data-id="'+ _arr[i].cityid +'">'+ _arr[i].city +'</option>'
+          }
+          $("#city").html(_html);
 
+        },
+        error: function(){}
+      })
     },
     getVerInSetting: function(){
       $.ajax({
