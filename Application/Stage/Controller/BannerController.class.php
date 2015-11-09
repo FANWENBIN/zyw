cf6136017796e50f4f2148ff8451ce752a91848c
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-11-09 14:53:04 +0800

    asdf

--- Application/Stage/Controller/BannerController.class.php
+++ Application/Stage/Controller/BannerController.class.php
@@ -396,14 +396,14 @@ class BannerController extends ComController {
                 $data['img'] = I('post.photo'.$i);
                 $data['newsid']  = I('post.newsid'.$i);
                 if(!empty($data['title']) && !empty($data['img'])){
-                    $image = new \Think\Image(); 
-                    $image->open('./Uploads'.$data['img']);
-                    $able = explode('.',$data['img']);
-                    //按照原图的比例生成一个最大为150*150的缩略图并保存为thumb.jpg   1200  470   
-                    $urlimg = './Uploads/small/'.date('YmdHis').rand(1000,99999).'.'.$able[1];  
-                    $image->thumb(120,47)->save($urlimg);
-                    //,\Think\Image::IMAGE_THUMB_SCALE   /small/banner/2015-09/144361715165112.jpg
-                    $data['smallimg'] = $urlimg;
+                    // $image = new \Think\Image(); 
+                    // $image->open('./Uploads'.$data['img']);
+                    // $able = explode('.',$data['img']);
+                    // //按照原图的比例生成一个最大为150*150的缩略图并保存为thumb.jpg   1200  470   
+                    // $urlimg = './Uploads/small/'.date('YmdHis').rand(1000,99999).'.'.$able[1];  
+                    // $image->thumb(120,47)->save($urlimg);
+                    // //,\Think\Image::IMAGE_THUMB_SCALE   /small/banner/2015-09/144361715165112.jpg
+                    // $data['smallimg'] = $urlimg;
                     $data['type'] = 9;
                     $sign = $banner->add($data);
                     if(!$sign){
