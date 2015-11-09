edf2407a36e8ac108c10aa3421ec4206c9ac2198
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-11-09 15:03:34 +0800

    asdf

--- Application/Home/Controller/VideoController.class.php
+++ Application/Home/Controller/VideoController.class.php
@@ -42,6 +42,7 @@ class VideoController extends ComController {
     $vedioval = $vedio->where('id='.$id)->find();
     session('video',$vedioval);
     $this->assign('list',$vedioval);
+
     $this->assign('sign',9);
     //评论显示
     $comment = M('comment');
@@ -53,11 +54,11 @@ class VideoController extends ComController {
     $Page       = new \Think\Page($count,25); // 实例化分页类 传入总记录数和每页显示的记录数(25)
     $show       = $Page->show(); //分页显示输出
     // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
-    $list = $comment->field('id,name,namehead,content,instime,')->where($data)->order('instime')->limit($Page->firstRow.','.$Page->listRows)->select();
+    $list = $comment->field('id,name,namehead,content,instime')->where($data)->order('instime')->limit($Page->firstRow.','.$Page->listRows)->select();
 
-    $this->assign('list',$list); //赋值数据集
+    $this->assign('lists',$list); //赋值数据集
     $this->assign('page',$show); //赋值分页输出
-
+   
     $this->display();
   }
    //视频汇
