7fb2e59dcecfdf1f040671c8976a8a44ea08a4f9
yangronghai <ronghai.yang@nadoo.cn>
2015-11-11 14:11:24 +0800

    update login face

--- Public/statics/src/js/rice/stick.js
+++ Public/statics/src/js/rice/stick.js
@@ -4,11 +4,69 @@ $(function(){
   };
   var page = {
     init: function(){
-      $(".comment").on("click",".itemreplybutton",page.reply);
-      $(".comment").on("click",".replybutton",page.subreply)
+      $(".comment").on("click",".itemreplybutton",page.replycomment);
+      $(".comment").on("click",".replybutton",page.subreply);
+      $("#mainreply").on("click",page.submit);
+      $(".inputsection form").on("submit",page.subSubmit)
     },
-    reply: function(){
+    subSubmit: function(){
+      var _this = this;
+      $.ajax({
+        type: "post",
+        url: "./index.php?m=Home&c=Rice&a=comment",
+        data: {
+          fid: $(_this).data("id"),
+          postid: $(".webmain").data("id"),
+          content: $(".replytext").val()
+        },
+        dataType: "json",
+        success: function(json){
+          if(json.status === 0){
+            console.log(json.msg);
+            window.location.reload()
+          }else if(json.status === 101){
+            console.log(json.msg);
+            alert("发布失败，请稍后再试")
+          }else if(json.status === 102){
+            console.log(json.msg);
+            alert("请登录或稍后再试")；
+          }
+        },
+        error: function(){
+
+        }
+      })
+    },
+    submit: function(){
+      $.ajax({
+        type: "post",
+        url: "./index.php?m=Home&c=Rice&a=comment",
+        data: {
+          fid: 0,
+          postid: $(".webmain").data("id"),
+          content: $("#mainreplycontent").val()
+        },
+        dataType: "json",
+        success: function(json){
+          if(json.status === 0){
+            console.log(json.msg);
+            window.location.reload()
+          }else if(json.status === 101){
+            console.log(json.msg);
+            alert("发布失败，请稍后再试")
+          }else if(json.status === 102){
+            console.log(json.msg);
+            alert("请登录或稍后再试")；
+          }
+        },
+        error: function(){
+
+        }
+      })
+    },
+    replycomment: function(){
       $(this).parents(".outeritem").find(".inputsection").show().find(".replytext")[0].focus();
+      $(this).parents(".outeritem").find(".replytext").val("")
     },
     subreply: function(){
       $(this).parents(".outeritem").find(".replytext").val("@"+$(this).parents(".itemreply").data("name")+" ")
