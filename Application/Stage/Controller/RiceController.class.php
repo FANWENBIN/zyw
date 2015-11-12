fdae9480b06a1c2c9565398c41c850f993be314f
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-11-12 11:15:50 +0800

    adf

--- Application/Stage/Controller/RiceController.class.php
+++ Application/Stage/Controller/RiceController.class.php
@@ -11,9 +11,10 @@ class RiceController extends Controller {
     $Page       = new \Think\Page($count,25);// 实例化分页类 传入总记录数和每页显示的记录数(25)
     $show       = $Page->show();// 分页显示输出
     // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
-    $this->list = $fans->order('fanssum desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+    $list = $fans->order('fanssum desc')->limit($Page->firstRow.','.$Page->listRows)->select();
     $this->assign('page',$show);// 赋值分页输出
 
+    $this->list = $list;
 
     $this->cur = 14;
     $this->display();
@@ -61,8 +62,64 @@ class RiceController extends Controller {
   *@version 2015年11月10日15:39:21
   */
   public function posts(){
+    $fans_posts = M('fans_posts');
+    $fans_club  = M('fans_club');
+
+    //$postslist  = $fans_posts->order('instime desc')->select();
+    $count = $fans_posts->count();
+    $page  = new \Think\Page($count,15);
+    $show  = $page->show();
+    $postslist = $fans_posts->order('instime desc')->limit($page->firstRow.','.$page->listRows)->select();
+    $this->page = $show;
+    foreach ($postslist as $key => $value) {
+      $clubval = $fans_club->where('id='.$value['fansclubid'])->find();
+      $postslist[$key]['fansclubid'] = $clubval['name'];
+    }
+    
+
+    $this->list = $postslist;
+   
     $this->cur = 14;
     $this->display();
+  }
+  /*帖子修改*/
+  public function postsedit(){
+    $submit = I('post.submit');
+    $id = I('get.id');
+    $fans = M('fans_posts');
+    $fans_club  = M('fans_club');
+    $actors = M('actors');
+    if(empty($submit)){
+      if(!empty($id)){
+        $where['id'] = $id;
+        $list = $fans->where($where)->find();
+      }
+     $this->clubval = $fans_club->where('id='.$list['fansclubid'])->find();
+     $this->list = $list;
+      //$this->actor = $actors->field('id,name')->where('status <> 0 and status <> 3')->order('initial')->select();
+      $this->cur = 14;
+      $this->display();
+    }else{
+      $data['title']   = I('post.title');
+      $data['content'] = I('post.content');
+
+      preg_match_all('/src=&quot;(.*?(\.jpg|\.jpeg|\.gif|\.png))/',$data['content'], $strs);
+        $data['img'] = '';
+        foreach($strs[1] as $key=>$val){
+            $data['img'] .= $val.',';               //加入图片
+        }
+
+
+      $id = I('get.id');
+      
+      $sign = $fans->where('id='.$id)->save($data);
+      
+      if($sign){
+        $this->success('操作成功',U('posts'));
+      }else{
+        $this->error('操作失败');
+      }
+    }
   }
   /**
   *论坛评论回复
