34f64422df0bd2a401c69c9b5a53fd1e43cf2d25
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-11-13 18:54:05 +0800

    sdaf

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -210,8 +210,8 @@ class UserController extends ComController {
         $Page       = new \Think\Page($count,25);// 实例化分页类 传入总记录数和每页显示的记录数(25)
         $show       = $Page->show();// 分页显示输出
         // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
-        $this->syslist = $msg->where($data)->order('instime desc')->limit($Page->firstRow.','.$Page->listRows)->select();
-        $this->assign('page',$show);// 赋值分页输出
+       // $this->syslist = $msg->where($data)->order('instime desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+        //$this->assign('page',$show);// 赋值分页输出
 
 
     	$data['type'] = 2; // 帖子评论回复消息
@@ -230,6 +230,20 @@ class UserController extends ComController {
         }
         $this->userlist = $userlist;
     	$this->display();
+    }
+    public function myinfo_news_system(){
+        $msg = M('user_msg');
+        $data['status'] = array('neq',0);
+        $data['instime'] = array('elt',time());
+        $data['uid'] = session('userid');
+        $data['type'] = 1; //系统消息
+        $count      = $msg->where($data)->count();// 查询满足要求的总记录数
+        $Page       = new \Think\Page($count,25);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+        $show       = $Page->show();// 分页显示输出
+        // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+        $this->syslist = $msg->where($data)->order('instime desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+        $this->assign('page',$show);// 赋值分页输出
+        $this->display();
     }
     /**
 	*我关注的饭团
