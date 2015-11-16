2b600b5c9c99b3156c517f61e83a225005d5fff8
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-11-16 19:21:39 +0800

    asdf

--- Public/statics/js/uploadify/uploadify.php
+++ Public/statics/js/uploadify/uploadify.php
@@ -6,7 +6,7 @@ Released under the MIT License <http://www.opensource.org/licenses/mit-license.p
 */
 
 // Define a destination
-$targetFolder = '/Uploads'; // Relative to the root
+$targetFolder = 'zyw/Uploads/fans'; // Relative to the root
 
 //$verifyToken = md5('unique_salt'.$_POST['timestamp']);
 
@@ -14,13 +14,17 @@ $targetFolder = '/Uploads'; // Relative to the root
 if (!empty($_FILES)) {
 	$tempFile = $_FILES['Filedata']['tmp_name'];
 	$targetPath = $_SERVER['DOCUMENT_ROOT'] . $targetFolder;
+	if(!is_dir($targetPath)){
+            mkdir($targetPath,0777,true);
+    }
 	$targetFile = rtrim($targetPath,'/') . '/' . $_FILES['Filedata']['name'];
-	
+	//var_dump($targetFile);die();
 	// Validate the file type
 	$fileTypes = array('jpg','jpeg','gif','png'); // File extensions
 	$fileParts = pathinfo($_FILES['Filedata']['name']);
 	
 	if (in_array($fileParts['extension'],$fileTypes)) {
+		var_dump($tempFile);
 		move_uploaded_file($tempFile,$targetFile);
 		$data['status'] = 0;
 		$data['msg']  = '';
