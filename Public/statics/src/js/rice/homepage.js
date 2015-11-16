532d176ee29311b00997b5360891794d403664bb
galen <249254832@qq.com>
2015-11-16 13:44:07 +0800

    update riceall

--- Public/statics/src/js/rice/homepage.js
+++ Public/statics/src/js/rice/homepage.js
@@ -1,14 +1,14 @@
-$(function(){
+$(function() {
   var scope = {
     isJoin: false
   };
   var page = {
-    init: function(){
-      $(".join").on("click",page.join);
-      $(".submitmessage").on("click",page.submit);
+    init: function() {
+      $(".join").on("click", page.join);
+      $(".submitmessage").on("click", page.submit);
       page.testJoin()
     },
-    submit: function(){
+    submit: function() {
       // 取得HTML内容
       var html = editor.html();
 
@@ -16,9 +16,13 @@ $(function(){
       editor.sync();
       html = $('#editor_id').val(); // jQuery
       var title = $(".articletitle").val();
-      console.log(title,html);
-      if(scope.isJoin){
-        $.ajax({
+      console.log(title, html);
+      // if(scope.isJoin){
+      if (!html || !title) {
+        alert("请确认该贴标题或内容是否填写！");
+        return false;
+      }
+      $.ajax({
           type: "post",
           url: "./index.php?m=Home&c=Rice&a=postini",
           data: {
@@ -27,28 +31,28 @@ $(function(){
             content: html
           },
           dataType: "json",
-          success: function(json){
-            if(json.status === 0){
+          success: function(json) {
+            if (json.status === 0) {
               console.log(json.msg);
               window.location.reload();
-            }else if(json.status === 101){
+            } else if (json.status === 101) {
               console.log(json.msg);
               alert("发送失败，请稍后再试");
-            }else if(json.status === 102){
+            } else if (json.status === 102) {
               console.log(json.msg);
-              alert("请填写全相关信息");
+              alert("请先登录后再发帖");
             }
           },
-          error: function(){
+          error: function() {
 
           }
 
         })
-      }else{
-        alert("请先加入饭团")
-      }
+        // }else{
+        //   alert("请先登陆并加入饭团");
+        // }
     },
-    join: function(){
+    join: function() {
       $.ajax({
         type: "get",
         url: "./index.php?m=Home&c=Rice&a=joinfans",
@@ -56,30 +60,30 @@ $(function(){
           fansid: $(".webmain").data("id")
         },
         dataType: "json",
-        success: function(json){
-          if(json.status === 0){
+        success: function(json) {
+          if (json.status === 0) {
             console.log(json.msg);
             $(".join").html("已入团");
-          }else if(json.status === 101){
+          } else if (json.status === 101) {
             console.log(json.msg)
             alert("加入失败，请稍后再试")
-          }else if(json.status === 102){
+          } else if (json.status === 102) {
             console.log(json.msg)
             alert("请先登陆后再尝试")
-          }else if(json.status === 103){
+          } else if (json.status === 103) {
             console.log(json.msg)
             alert("加入失败，请稍后再试")
-          }else if(json.status === 104){
+          } else if (json.status === 104) {
             console.log(json.msg);
             $(".join").html("已入团");
           }
         },
-        error: function(){
+        error: function() {
 
         }
       })
     },
-    testJoin: function(){
+    testJoin: function() {
       $.ajax({
         type: "get",
         url: "./index.php?m=Home&c=Rice&a=checkjoin",
@@ -87,17 +91,17 @@ $(function(){
           fansid: $(".webmain").data("id")
         },
         dataType: "json",
-        success: function(json){
-          if(json.status === 1){
+        success: function(json) {
+          if (json.status === 1) {
             scope.isJoin = true;
             $(".join").html("已入团");
             console.log(json.msg);
-          }else if(json.status === 0){
+          } else if (json.status === 0) {
             $(".join").html("+ 入团");
             console.log(json.msg);
           }
         },
-        error: function(){
+        error: function() {
 
         }
       })
