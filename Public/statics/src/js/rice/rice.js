2558722f52dc178bc60eaad2b41720d6c76086d5
galen <249254832@qq.com>
2015-11-16 16:13:47 +0800

    update createRice

--- Public/statics/src/js/rice/rice.js
+++ Public/statics/src/js/rice/rice.js
@@ -5,8 +5,10 @@ $(function(){
   var page = {
     init:function(){
       $("#community .group").on("click","li",page.tabCommunity);
+
       tabinit();
     },
+    
     tabCommunity: function(){
       var _index = $(this).index();
       $("#community .group").find("li").removeClass("active")
