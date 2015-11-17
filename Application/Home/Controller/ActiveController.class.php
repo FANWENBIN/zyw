2a68044a3232e5ce7fc9fc0c658646484a9bab5f
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-11-17 17:53:27 +0800

    sadf

--- Application/Home/Controller/ActiveController.class.php
+++ Application/Home/Controller/ActiveController.class.php
@@ -129,9 +129,11 @@ class ActiveController extends ComController {
     //用户发起活动
     public function useraddactive(){
         $a = $this->checklogin();  //验证登陆
+        
         if($a){
-            ajaxReturn();
+            ajaxReturn(104,'未登录','');
         }
+        $active = M('active');
         // $upload = new \Think\Upload();// 实例化上传类    
         // $upload->maxSize   =     3145728 ;// 设置附件上传大小    
         // $upload->exts      =     array('jpg', 'png', 'jpeg');
@@ -143,6 +145,7 @@ class ActiveController extends ComController {
         // }else{
         // $data['img'] = $info['mypic']['savepath'].$info['mypic']['savename'];
         // }
+
         $data['img'] = substr(stripslashes(I('post.img')), 9);
         $data['title']        = I('post.title');
         $data['content']      = I('post.content');
