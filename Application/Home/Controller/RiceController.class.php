348c32e56d2b413b7d9f239ff73814462d10bfc4
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-11-17 12:17:37 +0800

    adsf

--- Application/Home/Controller/RiceController.class.php
+++ Application/Home/Controller/RiceController.class.php
@@ -126,18 +126,19 @@ class RiceController extends ComController {
         //$list = $this->sortOut($commentlist,0,0,'---','fid','id');
        // $flist = $fans_comment->order('instime asc')->where('postid = '.$postslist['id'].' and status = 1 and fid = 0')->select();  //评论列表
 
-        $count      = $fans_comment->where('postid = '.$postslist['id'].' and status = 1 and fid = 0')->count();// 查询满足要求的总记录数
+        $count      = $fans_comment->where('postid = '.$postslist['id'].' and status <> 0 and fid = 0')->count();// 查询满足要求的总记录数
         $Page       = new \Think\Page($count,5);// 实例化分页类 传入总记录数和每页显示的记录数(25)
         $show       = $Page->show();// 分页显示输出
         // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
-        $flist = $fans_comment->order('instime asc')->where('postid = '.$postslist['id'].' and status = 1 and fid = 0')->limit($Page->firstRow.','.$Page->listRows)->select();
+        $flist = $fans_comment->order('instime asc')->where('postid = '.$postslist['id'].' and status <> 0 and fid = 0')->limit($Page->firstRow.','.$Page->listRows)->select();
         $this->assign('page',$show);// 赋值分页输出
 
         foreach ($flist as $key => $value) {
-            $array['flist'] = $value;
-            $array[$key] = $this->loopreply($flist,$value,$postslist);
+            $list[$key]['flist'] = $value;
+            $list[$key]['slist'] = $this->loopreply($value,$postslist);
         }
-       
+        
+       var_dump($list);
 
         //推荐活动
         $active = M('active');
@@ -255,6 +256,7 @@ class RiceController extends ComController {
         $data['headimg'] = session('userimg');
         $result = $this->checkDump($data);
         if($result == 0){ajaxReturn(102,'参数不可为空',$data);}
+        $data['cid'] = I('post.cid');
         $data['fid']    = I('post.fid'); 
         $fans_comment = M('fans_comment');     //论坛评论表
         $user_msg = M('user_msg');             //用户消息表
