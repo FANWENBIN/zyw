7023d22ba6cbcf9f40f351a99b018615d1e34810
songxiaoxiao <xiaoxiao.song@nadoo.cn>
2015-11-20 18:05:01 +0800

    asdf

--- libweibo/config.php
+++ libweibo/config.php
@@ -3,4 +3,6 @@ header('Content-Type: text/html; charset=UTF-8');
 
 define( "WB_AKEY" , '2165049105' );
 define( "WB_SKEY" , '558a5c37451069d4e8bbf92fb104c1f1' );
-define( "WB_CALLBACK_URL" , 'http://m2.nadoo.cn/p/zyw/index.php/Home/Login/weibologin' );
+// define( "WB_CALLBACK_URL" , 'http://m2.nadoo.cn/p/zyw/index.php/Home/Login/weibologin' );
+
+define( "WB_CALLBACK_URL" , 'http://m2.nadoo.cn/p/zyw/libweibo/callback.php' );
